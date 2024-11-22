ecdce7d9b36bb90c09edc8b62f76d57c91a177e2
jackie <as212638@gmail.com>
2024-11-22 17:32:09 +0800

    1122repository

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	// 預設對自動實現 CRUD
+	// 用 email 查詢 user (自動產生 SQL)
+	User findByEmail(String email);
+}
